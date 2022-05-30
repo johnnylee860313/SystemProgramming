a344795d5ad05f9acb27a764e1d2eaae0876e012
Johnny Lee <bluesky19970313@gmail.com>
2022-05-30 20:35:39 +0800

    functl

---
+++ fcntl/fcntl.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<string.h>
+#include<fcntl.h>
+
+int main(int argc,char *argv[]){
+    int val;
+
+    if(argc != 2){
+        err_quit("rsage : a.out <descriptor>");
+    }
+
+    if((val = fcntl(atoi(argv[1]),F_GETFL,0)) < 0){
+        err_sys("fcntl err for fd %d",atoi(argv[1]));
+    }
+
+    switch(val & O_ACCMODE){
+        case O_RDONLY :
+            printf("read only");
+            break;
+        case O_WRONLY :
+            printf("write only");
+            break;
+        case O_RDWR :
+            printf("both read write allowed");
+            break;
+        default :
+            err_dump("unknown access mode");
+    }
+
+    if(val & O_APPEND)
+        printf(" , append");
+    
+    if(val & O_NONBLOCK)
+        printf(" , nonblocking");
+    
+    if(val & O_SYNC)
+        printf(" , synchronous writes");
+
+#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC) && (O_FSYNC != OSYNC)
+    if(val & O_FSYNC)
+        printf(" , synchronous writes");
+#endif
+
+    
+    exit(0);
+}
